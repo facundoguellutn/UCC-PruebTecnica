04b43620f766ec3351e80c6b602f02ccdd7f3bc5
facundoelcapo <105519740+facundoguellutn@users.noreply.github.com>
2023-10-11 11:55:01 -0300

    login

--- client/app-frontend/src/pages/Login.jsx
+++ client/app-frontend/src/pages/Login.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import escudo from "../images/escudo.png"
+import { InputText } from "primereact/inputtext";
+import { Password } from 'primereact/password';
+import { Navigate, useNavigate } from "react-router-dom";
+
 
 const Login = () => {
+  let navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    navigate("/usuarios")
+  }
   return (
-    <div className='w-screen h-screen items-start justify start pt-[100px]'>Login</div>
+    <div className='w-screen h-screen flex flex-col items-center justify-center'>
+      <img src={escudo} alt="" className='mb-4' />
+      <form onSubmit={handleSubmit} className='flex items-center justify-center flex-col py-4 px-4 rounded-[20px] border-[1px] border-[#BCBCBE] w-[250px] sm:w-[370px]'>
+        <h1 className='text-blue500 text-[26px] mb-4'>Iniciar sesion</h1>
+        <InputText value={values.email} placeholder='Email' name='email' required={true} type='email' onChange={(e) => handleChange(e)} className='w-full' style={{ marginBottom: "15px" }} />
+        <InputText value={values.password} placeholder='Contraseña' name='password' required={true} type='password' onChange={(e) => handleChange(e)} className='w-full' />
+        <button className='mt-6 w-full bg-blue500 text-white py-2 rounded-[10px] text-[24px]'>Ingresar</button>
+        <h1 className='mt-4 text-blue500'>No tenes una cuenta?<span className='text-blue200 cursor-pointer' onClick={()=>{navigate("registro")}}> Registrate aqui</span> </h1>
+      </form>
+
+    </div>
   )
 }
 
